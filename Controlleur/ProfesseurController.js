e799293f3c2562ccfe971a2a9018a6e6f26f594a
Nick Mathieu <rakotomanananick29@gmail.com>
2024-03-19 18:05:47 +0300

    [BACK] CRUD Matiere (INSERT,GET)

--- Controlleur/ProfesseurController.js
+++ Controlleur/ProfesseurController.js
@@ -8,3 +8,22 @@ exports.getProfesseur = async (req, res) => {
     });
   } catch (err) {}
 };
+
+exports.listeMatiere = async (req, res) => {
+  try {
+    let posts = await professeurRepository.listeMatiere(req.query.page, req.query.pageNumber, res);
+    res.status(200).json({
+      status: 200,
+      data: posts,
+    });
+  } catch (err) {}
+};
+
+exports.insertMatiere = async (req, res) => {
+  professeurRepository
+    .insertionMatiere(req.params.idProf,req.body.libelle,req.body.idNiveau,req.body.photo,res)
+    .then((result) => res.status(200).json({ result }))
+    .catch();
+};
+
+
--- routes/Professeur.js
+++ routes/Professeur.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const professeurControlleur = require('../Controlleur/ProfesseurController')
  
 router.get('/', professeurControlleur.getProfesseur);
+router.get("/matieres", professeurControlleur.listeMatiere);
+router.put('/:idProf', professeurControlleur.insertMatiere);
 
 module.exports = router;
